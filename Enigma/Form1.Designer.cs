e6ad3a0b6831cd3e064f70175820e03aebd04869
Oatstar <76726117+Oatstar@users.noreply.github.com>
2023-04-22 14:56:50 +0300

    First commit

---
+++ Enigma/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Enigma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBoxRotor1 = new System.Windows.Forms.TextBox();
+            this.textBoxRotor2 = new System.Windows.Forms.TextBox();
+            this.textBoxRotor3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rotorSettingsLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(103, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Encrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 49);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 45);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ENCRYPTED OUTPUT:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "___";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(592, 361);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 31);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Reset Rotors";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBoxRotor1
+            // 
+            this.textBoxRotor1.Location = new System.Drawing.Point(103, 283);
+            this.textBoxRotor1.Name = "textBoxRotor1";
+            this.textBoxRotor1.Size = new System.Drawing.Size(34, 20);
+            this.textBoxRotor1.TabIndex = 10;
+            // 
+            // textBoxRotor2
+            // 
+            this.textBoxRotor2.Location = new System.Drawing.Point(103, 309);
+            this.textBoxRotor2.Name = "textBoxRotor2";
+            this.textBoxRotor2.Size = new System.Drawing.Size(34, 20);
+            this.textBoxRotor2.TabIndex = 11;
+            // 
+            // textBoxRotor3
+            // 
+            this.textBoxRotor3.Location = new System.Drawing.Point(103, 335);
+            this.textBoxRotor3.Name = "textBoxRotor3";
+            this.textBoxRotor3.Size = new System.Drawing.Size(34, 20);
+            this.textBoxRotor3.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(143, 312);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "ROTOR 2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(143, 338);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "ROTOR 3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(103, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "ROTOR SETTINGS";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(103, 361);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "SAVE";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.Location = new System.Drawing.Point(345, 9);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(94, 25);
+            this.headerLabel.TabIndex = 18;
+            this.headerLabel.Text = "ENIGMA";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(500, 160);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(25, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "___";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(500, 134);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(118, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "CLEARTEXT OUTPUT";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(503, 49);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 44);
+            this.textBox2.TabIndex = 23;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(503, 100);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "Decrypt";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(592, 324);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(180, 31);
+            this.button7.TabIndex = 27;
+            this.button7.Text = "Spinrotor (Debug)";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(143, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "ROTOR 1";
+            // 
+            // rotorSettingsLabel
+            // 
+            this.rotorSettingsLabel.AutoSize = true;
+            this.rotorSettingsLabel.Location = new System.Drawing.Point(103, 400);
+            this.rotorSettingsLabel.Name = "rotorSettingsLabel";
+            this.rotorSettingsLabel.Size = new System.Drawing.Size(61, 13);
+            this.rotorSettingsLabel.TabIndex = 30;
+            this.rotorSettingsLabel.Text = "_________";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(494, 400);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(278, 13);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Remember to Reset Rotors after encrypting or decrypting!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rotorSettingsLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.headerLabel);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxRotor3);
+            this.Controls.Add(this.textBoxRotor2);
+            this.Controls.Add(this.textBoxRotor1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBoxRotor1;
+        private System.Windows.Forms.TextBox textBoxRotor2;
+        private System.Windows.Forms.TextBox textBoxRotor3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label rotorSettingsLabel;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Enigma/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enigma
+{
+    public partial class Form1 : Form
+    {
+        public string inputStringCleartext;
+        public string outputStringEncrypted;
+
+        public string inputStringEncrypted;
+        public string outputStringCleartext;
+
+        public int rotorLength = 26;
+        public int[] rotorSettings = new int[] {0,0,0 };
+
+
+        int[,] rotorConnectionsDefault = new int[3, 26]
+            {
+                {1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14  },
+                {1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14  },
+                {1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14  }
+            };
+
+        int[,] rotorConnections = new int[3, 26]
+            {
+                {1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14  },
+                {1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14  },
+                {1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14  }
+            };
+                                        // 0    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25
+        char[] alphabets = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void FirstRotor()
+        {
+            //TESTING THE FUNCTION
+            string debugText = "";
+            for (int i = 0; i < rotorLength; i++)
+            {
+                debugText += " " + rotorConnections[0, i];
+            }
+
+            Debug.WriteLine("------");
+            Debug.WriteLine("Rotor0Connections: " + debugText);
+
+            foreach (char c in inputStringCleartext)
+            {
+                if (c == ' ')
+                    outputStringEncrypted += c;
+
+                for (int i = 0; i < alphabets.Length; i++)
+                {
+                    if (c == alphabets[i])
+                    {
+                        int rotor0Value = rotorConnections[0, i];
+                        int rotor1Value = rotorConnections[1, rotor0Value];
+                        int rotor2Value = rotorConnections[2, rotor1Value];
+
+                        outputStringEncrypted += alphabets[rotor2Value];
+                        
+                        Debug.WriteLine("Rotor0 value is: " + rotor0Value);
+                        Debug.WriteLine("Rotor1 value is: " + rotor1Value);
+                        Debug.WriteLine("Rotor2 value is: " + rotor2Value);
+                        Debug.WriteLine(c + " = " + alphabets[rotorConnections[1, i]]);
+                        SpinRotor(0);
+                    }
+                 }
+            }
+            label3.Text = outputStringEncrypted;
+        }
+
+        void SpinRotor(int rotorNumber)
+        {
+            int[,] tempRotorConnections = new int[3,rotorLength];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    tempRotorConnections[i, j] = rotorConnections[i, j];
+                }
+            }
+
+            for (int i = 0; i < rotorLength; i++)
+            {
+                if (i == 25)
+                    rotorConnections[rotorNumber, i] = tempRotorConnections[rotorNumber, 0];
+                else
+                    rotorConnections[rotorNumber, i] = tempRotorConnections[rotorNumber, i + 1];
+            }
+
+            if (rotorConnections[rotorNumber,0] == rotorConnectionsDefault[rotorNumber,0])
+            {
+                if(rotorNumber == 3)
+                    SpinRotor(1);
+                else
+                    SpinRotor(rotorNumber+1);
+            }
+                
+            //TESTING THE FUNCTION
+            string debugText = "";
+            for (int i = 0; i < rotorLength; i++)
+            {
+                debugText += " " + rotorConnections[0, i];
+            }
+
+            Debug.WriteLine("------");
+            Debug.WriteLine("Rotor0Connections: "+ debugText);
+        }
+      
+        void ResetRotors()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    rotorConnections[i, j] = rotorConnectionsDefault[i, j];
+                }
+            }
+            textBoxRotor1.Text = "";
+            textBoxRotor2.Text = "";
+            textBoxRotor3.Text = "";
+
+            rotorSettingsLabel.Text = "";
+        }
+
+        void SetRotors()
+        {
+            //For each rotor (3 times)
+            for (int currentRotor = 0; currentRotor < 3; currentRotor++)
+            {
+
+                //Copy the default rotors to temporary array
+                int[,] tempRotorConnections = new int[3, rotorLength];
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 26; j++)
+                    {
+                        tempRotorConnections[i, j] = rotorConnectionsDefault[i, j];
+                    }
+                }
+
+                //For each index in rotor copy the defaults as new rotorConnections values
+                for (int i = 0; i < rotorLength; i++)
+                {
+                    if (i + rotorSettings[currentRotor] < 26)
+                        rotorConnections[currentRotor, i] = tempRotorConnections[currentRotor, i+rotorSettings[currentRotor]];
+                    else
+                        rotorConnections[currentRotor, i] = tempRotorConnections[currentRotor, i + rotorSettings[currentRotor] - rotorLength];
+                }
+
+                //TESTING THE FUNCTION
+                string debugText = "";
+                for (int i = 0; i < rotorLength; i++)
+                {
+                    debugText += " " + rotorConnections[currentRotor, i];
+                }
+
+                Debug.WriteLine("------");
+                Debug.WriteLine(debugText);
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //Start Encrypting
+            outputStringEncrypted = "";
+            inputStringCleartext = textBox1.Text;
+            FirstRotor();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //SpinRotor(1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ResetRotors();
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            rotorSettings[0] = int.Parse(textBoxRotor1.Text);
+            rotorSettings[1] = int.Parse(textBoxRotor2.Text);
+            rotorSettings[2] = int.Parse(textBoxRotor3.Text);
+            Debug.WriteLine("ASD " + textBoxRotor1.Text + ", " + textBoxRotor2.Text + ", " + textBoxRotor3.Text);
+            rotorSettingsLabel.Text = textBoxRotor1.Text + ", " + textBoxRotor2.Text + ", " + textBoxRotor3.Text;
+
+            SetRotors();
+        }
+
+
+        //Start Decrypting given text
+        private void button6_Click(object sender, EventArgs e)
+        {
+            inputStringEncrypted = textBox2.Text;
+            label11.Text = "";
+            DoDecrypting();
+        }
+
+        //
+        void DoDecrypting()
+        {
+            outputStringCleartext = "";
+
+            foreach (char c in inputStringEncrypted)
+            {
+                if (c == ' ')
+                    outputStringCleartext += c;
+
+                for (int i = 0; i < alphabets.Length; i++)
+                {
+                    if (c == alphabets[i])
+                    {
+                        int rotor0Index = 0;
+                        int rotor1Index = 0;
+                        int rotor2Index = 0;
+
+                        for (int j = 0; j < rotorLength; j++)
+                        {
+                            if (rotorConnections[2, j] == i)
+                                rotor2Index = j;
+                        }
+
+                        for (int j = 0; j < rotorLength; j++)
+                        {
+                            if (rotorConnections[1, j] == rotor2Index)
+                                rotor1Index = j;
+                        }
+
+                        for (int j = 0; j < rotorLength; j++)
+                        {
+                            if (rotorConnections[0, j] == rotor1Index)
+                                rotor0Index = j;
+                        }
+
+                        Debug.WriteLine("rotor2Index: " + rotor2Index);
+                        Debug.WriteLine("rotor1Index: " + rotor1Index);
+                        Debug.WriteLine("rotor0Index: " + rotor0Index);
+                        outputStringCleartext += alphabets[rotor0Index];
+                        Debug.WriteLine("outputStringClearText: " + outputStringCleartext);
+                        //Debug.WriteLine("Rotor0 value is: " + rotor0Value);
+                        //Debug.WriteLine("Rotor1 value is: " + rotor1Value);
+                        //Debug.WriteLine("Rotor2 value is: " + rotor2Value);
+                        //Debug.WriteLine(c + " = " + alphabets[rotorConnections[1, i]]);
+                        SpinRotor(0);
+                    }
+                    
+                }
+            }
+            label11.Text = outputStringCleartext;
+        }
+    }
+}
